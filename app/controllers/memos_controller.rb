88b49d1b9e866cedd1b5aa57d2c9f5536dce4eb0
zn-kaki <zn-kaki>
2019-02-19 15:28:17 +0900

    コントローラー

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,12 +2,25 @@ class MemosController < ApplicationController
   def index
   end
 
+  def create
+    memo = Memo.new(memo_params)
+    memo.save!
+    redirect_to memos_url, notice: "メモ「#{memo.name}」を登録しました。"
+  end
+
   def show
   end
 
   def new
+    @memo = Memo.new
   end
 
   def edit
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:name, :description)
   end
 end
